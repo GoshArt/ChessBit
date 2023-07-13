1ae6120bad90dfea57c27af03733b007b2474162
Jokand <nechaevgeorgui@gmail.com>
2023-07-13 16:29:21 +0300

    create fild

--- main/views.py
+++ main/views.py
@@ -9,7 +9,7 @@ import random
 def index(request):
     goshas = Gosha.objects.all()
     name = "Артём"
-    auth = True
+    auth = False
     id = 5
     url_avatar = 'main/img/art.jpg'
     return render(request, 'main/index.html', {"name": name, "url_avatar": url_avatar, "id": id, "auth": auth})
@@ -32,6 +32,56 @@ def profile(request):
 
 
 def servers(request):
+    lobbys = []
+    for i in range(10):
+        res = random.randint(200, 1600)
+        lobby = {"name": "Тёмыч", "rating": res, "avatar": "main/img/art.jpg", "type": "Classic", "game_id": "2"}
+        lobbys.append(lobby)
     url_avatar = 'main/img/art.jpg'
-    return render(request, 'main/servers.html', {"url_avatar": url_avatar})
+    return render(request, 'main/servers.html', {"lobbys": lobbys, "url_avatar": url_avatar})
 
+
+def field(request):
+    ser = []
+    pos_str = "rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR "
+    for i in range(64):
+        b = ""
+        if int(i / 8) % 2 == 0:
+            if (i + 2) % 2 == 0:
+                a = "sq_w"
+            else:
+                a = "sq_b"
+        else:
+            if (i + 2) % 2 == 0:
+                a = "sq_b"
+            else:
+                a = "sq_w"
+        match pos_str[i]:
+            case 'K':
+                b = '♔'
+            case 'Q':
+                b = '♕'
+            case 'R':
+                b = '♖'
+            case 'B':
+                b = '♗'
+            case 'N':
+                b = '♘'
+            case 'P':
+                b = '♙'
+            case 'k':
+                b = '♚'
+            case 'q':
+                b = '♛'
+            case 'r':
+                b = '♜'
+            case 'b':
+                b = '♝'
+            case 'n':
+                b = '♞'
+            case 'p':
+                b = '♟'
+            case '1':
+                b = ''
+        ser.append({"sq_c": a, 'pos_f' : b})
+    return render(request, 'main/field.html', {"ser": ser})
