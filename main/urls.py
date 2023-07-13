9fc0fc1e8be4797161f6020f22b850b0681bbcd2
Jokand <nechaevgeorgui@gmail.com>
2023-07-13 10:14:29 +0300

    create page layout

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('about', views.about),
+    path('profile', views.profile),
+    path('servers', views.servers),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,37 @@
+from random import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Gosha
+import random
 
 
 def index(request):
     goshas = Gosha.objects.all()
-    return render(request, 'main/index.html', {'goshas': goshas})
+    name = "Артём"
+    auth = True
+    id = 5
+    url_avatar = 'main/img/art.jpg'
+    return render(request, 'main/index.html', {"name": name, "url_avatar": url_avatar, "id": id, "auth": auth})
 
 
-def about(request):
-    return render(request, 'main/about.html')
+def profile(request):
+    games = []
+    for i in range(10):
+        res = random.randint(-1, 1)
+        game = {"name": "Тёмыч", "avatar": "main/img/art.jpg", "res": res, "date": "23.02.2022", "game_id": "2",
+                "history_id": "2"}
+        games.append(game)
+    id = request.GET.get("id")
+    name = "Артём"
+    email = "ArtemSurov@gmail.com"
+    reg = "11.09.2001"
+    url_avatar = 'main/img/art.jpg'
+    return render(request, 'main/profile.html',
+                  {"games": games, "id": id, "name": name, "url_avatar": url_avatar, "email": email, "reg": reg})
 
-# Create your views here.
+
+def servers(request):
+    url_avatar = 'main/img/art.jpg'
+    return render(request, 'main/servers.html', {"url_avatar": url_avatar})
+
