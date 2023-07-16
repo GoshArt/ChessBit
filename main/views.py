0b91f9298a8ed4efa47bb5652e76b2626ec443a4
ArtemSurovSergeevich <75212119+NokASur@users.noreply.github.com>
2023-07-16 09:42:10 +0300

    Some adjustments

--- main/views.py
+++ main/views.py
@@ -2,16 +2,69 @@ from random import random
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Gosha
+from .models import *
 import random
 
 
 def index(request):
-    goshas = Gosha.objects.all()
-    name = "Артём"
-    auth = True
+    auth = False
+    name = ""
     url_avatar = 'main/img/art.jpg'
-    return render(request, 'main/index.html', {"name": name, "url_avatar": url_avatar, "auth": auth})
+    error = ""
+
+    print(request.GET)
+    print(request.POST)
+
+    if request.method == "GET":
+        print("GET")
+        pass
+        # if "re_password" in request.GET:
+        #     print(request.GET)
+
+    elif request.method == "POST":
+        print("POST")
+        if "username" in request.POST:
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = User.objects.filter(UserNickname=username, UserPassword=password)
+
+            if user:
+                request.session['auth'] = True
+                request.session['name'] = username
+                request.session['password'] = password
+            else:
+                error = "Данные введены неверно"
+                print("Вы рофлите")
+        elif "re_password" in request.POST:
+            name = request.POST["name"]
+            email = request.POST["email"]
+            f_password = request.POST["f_password"]
+            re_password = request.POST["re_password"]
+
+            if f_password != re_password:
+                error = "Пароль хуйня"
+            else:
+                new_user = User()
+                new_user.UserNickname = name
+                new_user.UserPassword = f_password
+                new_user.UserEmail = email
+                new_user.save()
+                request.session['auth'] = True
+                request.session['name'] = name
+                request.session['password'] = f_password
+        elif len(request.POST) < 2:
+            request.session['auth'] = False
+            request.session['name'] = ""
+            request.session['password'] = ""
+            print("done")
+
+    if "auth" in request.session:
+        if request.session["auth"]:
+            auth = True
+            name = request.session["name"]
+
+    print(auth)
+    return render(request, 'main/index.html', {"name": name, "url_avatar": url_avatar, "auth": auth, "error": error})
 
 
 def waiting(request):
