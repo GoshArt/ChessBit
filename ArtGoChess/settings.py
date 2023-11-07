7f4dffcba823d34a71969766ea1ce50d2524296f
NokASur <asurov2017@litsey2.ru>
2023-11-08 01:11:50 +0300

    bruhlord6

--- ArtGoChess/settings.py
+++ ArtGoChess/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER': os.getenv("DATABASE_USER"),
         'PASSWORD': os.getenv("DATABASE_PASSWORD"),
         'HOST': os.getenv("DATABASE_HOST"),
-        'PORT': '5432'
+        'PORT': os.environ.get("DATABASE_PORT"),
     }
 }
 
